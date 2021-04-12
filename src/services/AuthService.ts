72086521623afa307d44941b02b8efdf17b7114a
quanghuy30101999 <huytran.301099@gmail.com>
2021-04-12 15:34:16 +0700

    Done login set default token in request headers

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,42 +1,34 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
-const API_URL = 'https://nova-logistics-backend.herokuapp.com/api/v1';
-
-const Login = async (email: string, password: string) => {
-  return await axios
-    .post(`${API_URL}/login`, { email, password })
-    .then((response) => {
-      localStorage.setItem('token', JSON.stringify(response.data.token));
-      localStorage.setItem('user', JSON.stringify(response.data.user));
-      setAuthToken(response.data.token);
-      return response.data;
-    });
-};
 
 type Token = {
   user_id: number;
   exp: number;
 };
 
+const API_URL = 'https://nova-logistics-backend.herokuapp.com/api/v1';
+
+const Login = async (email: string, password: string) => {
+  const response = await axios.post(`${API_URL}/login`, { email, password });
+  if (response.data) {
+    localStorage.setItem('token', response.data.token);
+    localStorage.setItem('user', response.data.user);
+    setAuthToken();
+    return response.data;
+  }
+  return response;
+};
+
 const isExpired = (): boolean => {
   const token = localStorage.getItem('token') || '';
   const decodedToken: Token = jwt_decode(token);
   const currentDate = new Date();
-
-  if (decodedToken.exp * 1000 < currentDate.getTime()) {
-    return false;
-  } else {
-    return true;
-  }
+  return decodedToken.exp * 1000 < currentDate.getTime() ? false : true;
 };
 
-const setAuthToken = (token: string): void => {
-  axios.defaults.headers.common[`Authorization`] = '';
-  delete axios.defaults.headers.common[`Authorization`];
-
-  if (token) {
-    axios.defaults.headers.common[`Authorization`] = `${token}`;
-  }
+const setAuthToken = (): void => {
+  const token = localStorage.getItem('token') || '';
+  axios.defaults.headers.common[`Authorization`] = `${token}`;
 };
 
 export default {
